112e0430c06682ec5f1cd49cd8194e8b3ca973cf
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-01 13:37:46 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -164,15 +164,19 @@ connect = 127.0.0.1:109
 
 [dropbear]
 accept = 2086
+connect = 127.0.0.1:109
+
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+
+[dropbear]
+accept = 777
 connect = 127.0.0.1:22
 
 [openvpn]
 accept = 442
 connect = 127.0.0.1:2200
-
-[openvpn]
-accept = 2095
-connect = 127.0.0.1:1194
 
 END
 
