f18c92ed0ec923cb83d8ad047b1031725cc31318
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-07-03 02:05:11 +0700

    Update edu1.py

--- edu1.py
+++ edu1.py
@@ -3,10 +3,7 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-if sys.argv[1:]:
-	LISTENING_PORT = sys.argv[1]
-else:
-	LISTENING_PORT = 2083
+LISTENING_PORT = 2083
 
 # Pass
 PASS = ''
@@ -14,7 +11,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:3413'
+DEFAULT_HOST = '127.0.0.1:110'
 RESPONSE = 'HTTP/1.1 101 Endka-Stores\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
