302b4ed3f2feaea497aa57c19018349d303585bd
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-09-07 02:19:11 +0700

    Update delws.sh

--- delws.sh
+++ delws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash             
-NUMBER_OF_CLIENTS=$(grep -c -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### Vmess " "/etc/nginx/conf.d/vps.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -12,7 +12,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 3-4 | nl -s ') '
+	grep -E "^### Vmess " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 3-4 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -20,9 +20,9 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### Vmess " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^### Vmess $user $exp/,/^}/d" /etc/nginx/conf.d/v2ray.conf
+user=$(grep -E "^### Vmess " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### Vmess " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### Vmess $user $exp/,/^}/d" /etc/nginx/conf.d/vps.conf
 systemctl disable v2ray@vmess-$user
 systemctl stop v2ray@vmess-$user
 rm -f /etc/v2ray/vmess-$user.json
