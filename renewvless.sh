36414e34ca900499ec0ffd42e6bdb3f560f7223b
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-09-07 16:15:55 +0700

    Update renewvless.sh

--- renewvless.sh
+++ renewvless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### Vless " "/etc/nginx/conf.d/vps.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -11,7 +11,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 3-4 | nl -s ') '
+	grep -E "^### Vless " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 3-4 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -20,15 +20,15 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf")
 		fi
 	done
 read -p "Expired (days): " masaaktif
-user=$(grep -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### Vless " "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### Vless " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### Vless " "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### Vless $user $exp/### Vless $user $exp4/g" /etc/nginx/conf.d/v2ray.conf
+sed -i "s/### Vless $user $exp/### Vless $user $exp4/g" /etc/nginx/conf.d/vps.conf
 clear
 echo ""
 echo " Akun VMESS berhasil diperpanjang"
