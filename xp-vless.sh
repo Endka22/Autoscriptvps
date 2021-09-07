8586b89d68ed78783e7c31d85de9bba90d9cbafb
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-09-07 16:17:46 +0700

    Update xp-vless.sh

--- xp-vless.sh
+++ xp-vless.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-data=( `cat /etc/nginx/conf.d/v2ray.conf | grep '^### Vless' | cut -d ' ' -f 3`);
+data=( `cat /etc/nginx/conf.d/vps.conf | grep '^### Vless' | cut -d ' ' -f 3`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### Vless $user" "/etc/nginx/conf.d/v2ray.conf" | cut -d ' ' -f 4)
+exp=$(grep -w "^### Vless $user" "/etc/nginx/conf.d/vps.conf" | cut -d ' ' -f 4)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^### Vless $user $exp/,/^}/d" /etc/nginx/conf.d/v2ray.conf
+sed -i "/^### Vless $user $exp/,/^}/d" /etc/nginx/conf.d/vps.conf
 rm -f /etc/v2ray/vless-$user.json
 systemctl stop v2ray@vless-$user
 systemctl disable v2ray@vless-$user
