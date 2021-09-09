cf4d0bd582b5cf7c52dc1312814d6b9de178e186
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-09-09 23:19:49 +0700

    Update inss-vt.sh

--- inss-vt.sh
+++ inss-vt.sh
@@ -26,185 +26,6 @@ chmod +x /root/.acme.sh/acme.sh
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 service squid start
 uuid=$(cat /proc/sys/kernel/random/uuid)
-cat> /etc/v2ray/config.json << END
-  {
-  "inbound": {
-    "port": 16888,
-    "listen":"127.0.0.1",
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
-        {
-          "id": "ac940e7d-b40f-4bb2-9a4b-0b1c6cc9e67d",
-          "level": 1
-#tls
-        }
-      ]
-    },
-     "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-         "path": "/endka"
-        }
-     }
-  },
-  "outbound": {
-    "protocol": "freedom",
-    "settings": {}
-  }
-}
-END
-cat> /etc/v2ray/none.json << END
-{
-  "inbounds": [
-    {
-      "port": 8080,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "$uuid"
-#none
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-        "path": "/endka2"
-        }
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
-}
-END
-cat> /etc/v2ray/vless.json << END
-  {
-  "inbounds": [
-    {
-      "port": 16999,
-      "listen":"127.0.0.1",
-      "protocol": "vless",
-      "settings": {
-        "decryption": "none",
-        "clients": [
-          {
-            "id": "1bb83c10-911e-4767-81d2-adb5177c0718",
-            "level": 0
-#tls
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-        "path": "/endka22"
-        }
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
- }
-END
-cat> /etc/v2ray/vnone.json << END
-{
-  "log": {
-    "access": "/var/log/v2ray/access2.log",
-    "error": "/var/log/v2ray/error.log",
-    "loglevel": "info"
-  },
-  "inbounds": [
-    {
-      "port": 8880,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "${uuid}"
-#none
-          }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "/v2ray",
-          "headers": {
-            "Host": ""
-          }
-         },
-        "quicSettings": {},
-        "sockopt": {
-          "mark": 0,
-          "tcpFastOpen": true
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      },
-      "domain": "$domain"
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  }
-}
-END
 cat <<EOF > /etc/trojan/config.json
 {
     "run_type": "server",
@@ -278,63 +99,47 @@ EOF
 cat <<EOF > /etc/trojan/uuid.txt
 $uuid
 EOF
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 16888 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 16999 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8880 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 16888 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 16999 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8880 -j ACCEPT
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
 systemctl daemon-reload
-systemctl enable v2ray@none.service
-systemctl start v2ray@none.service
-systemctl enable v2ray@vless.service
-systemctl start v2ray@vlessservice
-systemctl enable v2ray@vnone.service
-systemctl start v2ray@vnone.service
 systemctl restart trojan
 systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O add-ws "https://raw.githubusercontent.com/Endka22/Autosc/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/Endka22/Autosc/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/Endka22/Autosc/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/Endka22/Autosc/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/Endka22/Autosc/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/Endka22/Autosc/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/renew-tr.sh"
+wget -O addws "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/addws.sh"
+wget -O addvless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/addvless.sh"
+wget -O delws "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/delws.sh"
+wget -O delvless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/delvless.sh"
+wget -O cekws "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cekws.sh"
+wget -O cekvless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cekvless.sh"
+wget -O renewws "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/renewws.sh"
+wget -O renewvless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/renewvless.sh"
+wget -O renewtr "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/renewtr.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/xp-ws.sh"
+wget -O xp-vless "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/xp-vless.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cert.sh"
-wget -O add-wstls "https://raw.githubusercontent.com/Endka22/Autosc/main/add-wstls.sh"
-wget -O del-non "https://raw.githubusercontent.com/Endka22/Autosc/main/del-non.sh"
-wget -O renew-non "https://raw.githubusercontent.com/Endka22/Autosc/main/renew-non.sh"
-chmod +x add-ws
-chmod +x add-vless
-chmod +x add-tr
-chmod +x del-ws
-chmod +x del-vless
-chmod +x del-tr
-chmod +x cek-ws
-chmod +x cek-vless
-chmod +x cek-tr
-chmod +x renew-ws
-chmod +x renew-vless
-chmod +x renew-tr
+wget -O addtrgo "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/addtrgo.sh"
+wget -O deltrgo "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/deltrgo.sh"
+wget -O cektrgo "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/cektrgo.sh"
+wget -O xp-trgo "https://raw.githubusercontent.com/Endka22/Autoscriptvps/main/xp-trgo.sh"
+chmod +x addtrgo
+chmod +x deltrgo
+chmod +x cektrgo
+chmod +x xp-trgo
+chmod +x addws
+chmod +x addvless
+chmod +x delws
+chmod +x delvless
+chmod +x cekws
+chmod +x cekvless
+chmod +x renewws
+chmod +x renewtr
+chmod +x renewvless
+chmod +x xp-ws
+chmod +x xp-vless
 chmod +x certv2ray
-chmod +x del-non
-chmod +x add-wstls
-chmod +x renew-non
 cd
-rm -f ins-vt.sh
 mv /root/domain /etc/v2ray
+echo "59 23 * * * root xp-ws" >> /etc/crontab
+echo "59 23 * * * root xp-trgo" >> /etc/crontab
+echo "59 23 * * * root xp-vless" >> /etc/crontab
 
