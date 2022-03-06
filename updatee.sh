28acd106edc38e03a4f6052bb62fbd2337ae5ea1
Endka22 <74788605+Endka22@users.noreply.github.com>
2022-03-06 23:30:29 +0700

    ok3

--- updatee.sh
+++ updatee.sh
@@ -36,6 +36,13 @@ chmod +x del-xr
 chmod +x del-xvless
 chmod +x xp-xr
 chmod +x xp-xvless
+#enc
+shc -r -f add-xr -o add-xr
+shc -r -f add-xvless -o add-xvless
+shc -r -f del-xr -o del-xr
+shc -r -f del-xvless -o del-xvless
+shc -r -f xp-xr -o xp-xr
+shc -r -f xp-xvless -o xp-xvless
 echo "0 0 * * * root xp-xr" >> /etc/crontab
 echo "0 0 * * * root xp-xvless" >> /etc/crontab
 clear
